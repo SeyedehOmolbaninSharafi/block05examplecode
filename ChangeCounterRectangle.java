fbe3d2b46d3135db76e5008b5f92fe75c3af53ad
niklasbroberg <niklas.broberg@gmail.com>
2019-02-03 22:33:16 +0100

    Add files via upload

---
+++ ChangeCounterRectangle.java
@@ -0,0 +1,47 @@
+/**
+ * Created by Niklas on 2016-02-02.
+ *
+ * Block 2-2: Inheritance, when and how?
+ */
+public class ChangeCounterRectangle extends Rectangle {
+
+    /* Rectangle:
+      - width : int
+      - height : int
+      + getWidth() : int
+      + getHeight() : int
+      + getArea() : int
+      + setWidth(int) : void
+      + setHeight(int) : void
+      + setSize(int, int) : void
+     */
+
+    public ChangeCounterRectangle(int w, int h){
+        super(w,h);
+    }
+
+    private int nrOfWidthChanges = 0;
+    private int nrOfHeightChanges = 0;
+    public int getNrOfWidthChanges(){
+        return nrOfWidthChanges;
+    }
+    public int getNrOfHeightChanges() {
+        return nrOfHeightChanges;
+    }
+
+    @Override public void setWidth(int w){
+        super.setWidth(w);
+        nrOfWidthChanges += 1;
+    }
+    @Override public void setHeight(int h){
+        super.setHeight(h);
+        nrOfHeightChanges += 1;
+    }
+
+    @Override public void setSize(int w, int h){
+        super.setSize(w,h);
+        nrOfHeightChanges += 1;
+        nrOfWidthChanges += 1;
+    }
+
+}
---
+++ Rectangle.java
@@ -0,0 +1,23 @@
+/**
+ * Created by Niklas on 2016-02-02.
+ *
+ * Block 2-2: Inheritance, where and when
+ */
+public class Rectangle {
+
+    private int width;
+    private int height;
+
+    public Rectangle(int w, int h){
+        this.width = w;
+        this.height = h;
+    }
+
+    public int getWidth() { return width; }
+    public int getHeight() { return height; }
+    protected int getArea() { return width * height; }
+    public void setWidth(int w) { this.width = w; }
+    public void setHeight(int h) { this.height = h; }
+    public void setSize(int w, int h) { setWidth(w); setHeight(h);}
+
+}
