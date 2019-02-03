708719e195802ce33302685cfd172f90af9b13cf
niklasbroberg <niklas.broberg@gmail.com>
2019-02-03 22:46:22 +0100

    Add files via upload

---
+++ RoleplayPlatform/User.java
@@ -0,0 +1,19 @@
+package RoleplayPlatform;
+
+/**
+ * Created by Niklas on 2016-02-02.
+ *
+ * Block 2-2: Inheritance, when and how?
+ */
+public class User {
+
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
